ea45e80abb546856779b0c24edc5b46118c46ac3
kfarr <kieran.farr@gmail.com>
2017-02-18 01:54:39 -0800

    added characters and reference separate select bar component repo

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('aframe-gridhelper-component');
 require('aframe-animation-component');
-// require('aframe-text-component');
-require('./lib/aframe-select-bar.js');
+
 require('./lib/builder-controls.js');
 require('./lib/ground.js');
 require('./lib/skyGradient.js');
